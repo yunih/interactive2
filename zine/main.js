0e5432751a45226af2c0c31f1b19a783356761fd
YUNA <yunahwang@Yunas-MacBook-Pro.local>
2019-09-24 16:41:55 -0700

    h

--- zine/main.js
+++ zine/main.js
@@ -1,32 +1,43 @@
 
 
-let shapes = document.querySelectorAll('.draggyShape')
-let draggedEl = undefined
-
-
-for(let counter = 0; counter < shapes.length; counter = counter + 1){
-	let x = Math.random() * (window.innerWidth - shapes[counter].clientWidth)
-	let y = Math.random() * (window.innerHeight - shapes[counter].clientHeight)
-	shapes[counter].style.left = x + 'px'
-	shapes[counter].style.top = y + 'px'
+window.onload = function(){
 
 
 
-	shapes[counter].onmousedown = function(){
-		draggedEl = this
+
+
+
+	let shapes = document.querySelectorAll('.draggyShape')
+	let draggedEl = undefined
+
+
+	for(let counter = 0; counter < shapes.length; counter = counter + 1){
+		let x = Math.random() * (window.innerWidth - shapes[counter].clientWidth)
+		let y = Math.random() * (window.innerHeight - shapes[counter].clientHeight)
+		shapes[counter].style.left = x + 'px'
+		shapes[counter].style.top = y + 'px'
+
+
+
+		shapes[counter].onmousedown = function(){
+			draggedEl = this
+		}
 	}
-}
 
 
 
 
-document.onmousemove = function(event){
-		
+	document.onmousemove = function(event){
+			
 
-	draggedEl.style.top = event.clientY - draggedEl.clientHeight/2 + 'px'
-	draggedEl.style.left = event.clientX - draggedEl.clientWidth/2 + 'px'
+		draggedEl.style.top = event.clientY - draggedEl.clientHeight/2 + 'px'
+		draggedEl.style.left = event.clientX - draggedEl.clientWidth/2 + 'px'
 
-}
-document.onmouseup = function() {
-	draggedEl = undefined
+	}
+	document.onmouseup = function() {
+		draggedEl = undefined
+	}
+
+
+
 }
